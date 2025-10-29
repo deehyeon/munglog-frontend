50d75599a6a799f2971d0e34c0f9f7464ed33142
Jihyeon An <84323575+deehyeon@users.noreply.github.com>
2025-10-29 15:02:37 +0900

    Add AdoptionPostCreate to App.js routing

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Missing from './pages/Missing';
 import MissingPostCreate from './pages/MissingPostCreate';
 import ProtectedPostCreate from './pages/ProtectedPostCreate';
 import Adoption from './pages/Adoption';
+import AdoptionPostCreate from './pages/AdoptionPostCreate';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import VolunteerSignup from './pages/VolunteerSignup';
@@ -87,6 +88,7 @@ export default function App() {
         {currentPage === 'missing-post-create' && <MissingPostCreate setCurrentPage={setCurrentPage} />}
         {currentPage === 'protected-post-create' && <ProtectedPostCreate setCurrentPage={setCurrentPage} />}
         {currentPage === 'adoption' && <Adoption {...pageProps} />}
+        {currentPage === 'adoption-post-create' && <AdoptionPostCreate setCurrentPage={setCurrentPage} />}
         {currentPage === 'chat' && <Chat {...pageProps} />}
         {currentPage === 'login' && <Login setCurrentPage={setCurrentPage} setIsLoggedIn={setIsLoggedIn} setUserType={setUserType} />}
         {currentPage === 'signup' && <Signup setCurrentPage={setCurrentPage} />}
